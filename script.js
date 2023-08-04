f201bdb2df6ec81367830d370cd8ec16c8349975
anithaguntreddi07 <126941619+anithaguntreddi07@users.noreply.github.com>
2023-08-04 19:34:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,126 +1,206 @@
-function convert() {
-  var inputValue = document.getElementById("inputValue").value;
-  var unitFrom = document.getElementById("unitFrom").value;
-  var unitTo = document.getElementById("unitTo").value;
-  var result;
+var OptionType = document.getElementById('OptionType');
+var input = document.getElementById('input');
+var result = document.getElementById('result');
+var InputType = document.getElementById('InputType');
+var ResultType = document.getElementById('ResultType');
 
-  
-  if (unitFrom === "cm" && unitTo === "m") {
-    result = inputValue / 100; 
-  } else if (unitFrom === "cm" && unitTo === "km") {
-    result = inputValue / 100000; 
-  } else if (unitFrom === "cm" && unitTo === "inch") {
-    result = inputValue / 2.54; 
-  } else if (unitFrom === "cm" && unitTo === "ft") {
-    result = inputValue / 30.48;  
-  } else if (unitFrom === "cm" && unitTo === "yard") {
-    result = inputValue / 91.44; 
-  } else if (unitFrom === "cm" && unitTo === "mile") {
-    result = inputValue / 160934.4;
-  } else if (unitFrom === "cm" && unitTo === "nauticalMile") {
-    result = inputValue / 185200; 
-  } else if (unitFrom === "m" && unitTo === "cm") {
-    result = inputValue * 100; 
-  } else if (unitFrom === "m" && unitTo === "km") {
-    result = inputValue / 1000; 
-  } else if (unitFrom === "m" && unitTo === "inch") {
-    result = inputValue * 39.37; 
-  } else if (unitFrom === "m" && unitTo === "ft") {
-    result = inputValue * 3.281; 
-  } else if (unitFrom === "m" && unitTo === "yard") {
-    result = inputValue * 1.094; 
-  } else if (unitFrom === "m" && unitTo === "mile") {
-    result = inputValue / 1609.344; 
-  } else if (unitFrom === "m" && unitTo === "nauticalMile") {
-    result = inputValue / 1852;
-  } else if (unitFrom === "km" && unitTo === "cm") {
-    result = inputValue * 100000; 
-  } else if (unitFrom === "km" && unitTo === "m") {
-    result = inputValue * 1000; 
-  } else if (unitFrom === "km" && unitTo === "inch") {
-    result = inputValue * 39370.079; 
-  } else if (unitFrom === "km" && unitTo === "ft") {
-    result = inputValue * 3280.84; 
-  } else if (unitFrom === "km" && unitTo === "yard") {
-    result = inputValue * 1093.613; 
-  } else if (unitFrom === "km" && unitTo === "mile") {
-    result = inputValue / 1.609; 
-  } else if (unitFrom === "km" && unitTo === "nauticalMile") {
-    result = inputValue / 1.852; 
-  } else if (unitFrom === "inch" && unitTo === "cm") {
-    result = inputValue * 2.54; 
-  } else if (unitFrom === "inch" && unitTo === "m") {
-    result = inputValue / 39.37;
-  } else if (unitFrom === "inch" && unitTo === "km") {
-    result = inputValue / 39370.079;
-  } else if (unitFrom === "inch" && unitTo === "ft") {
-    result = inputValue / 12;
-  } else if (unitFrom === "inch" && unitTo === "yard") {
-    result = inputValue / 36; 
-  } else if (unitFrom === "inch" && unitTo === "mile") {
-    result = inputValue / 63360; 
-  } else if (unitFrom === "inch" && unitTo === "nauticalMile") {
-    result = inputValue / 72913.386;
-  } else if (unitFrom === "ft" && unitTo === "cm") {
-    result = inputValue * 30.48;
-  } else if (unitFrom === "ft" && unitTo === "m") {
-    result = inputValue / 3.281; 
-  } else if (unitFrom === "ft" && unitTo === "km") {
-    result = inputValue / 3280.84;
-  } else if (unitFrom === "ft" && unitTo === "inch") {
-    result = inputValue * 12; 
-  } else if (unitFrom === "ft" && unitTo === "yard") {
-    result = inputValue / 3; 
-  } else if (unitFrom === "ft" && unitTo === "mile") {
-    result = inputValue / 5280; 
-  } else if (unitFrom === "ft" && unitTo === "nauticalMile") {
-    result = inputValue / 6076.115;
-  } else if (unitFrom === "yard" && unitTo === "cm") {
-    result = inputValue * 91.44; 
-  } else if (unitFrom === "yard" && unitTo === "m") {
-    result = inputValue / 1.094;
-  } else if (unitFrom === "yard" && unitTo === "km") {
-    result = inputValue / 1093.613; 
-  } else if (unitFrom === "yard" && unitTo === "inch") {
-    result = inputValue * 36; 
-  } else if (unitFrom === "yard" && unitTo === "ft") {
-    result = inputValue * 3; 
-  } else if (unitFrom === "yard" && unitTo === "mile") {
-    result = inputValue / 1760; 
-  } else if (unitFrom === "yard" && unitTo === "nauticalMile") {
-    result = inputValue / 2025.372; 
-  } else if (unitFrom === "mile" && unitTo === "cm") {
-    result = inputValue * 160934.4; 
-  } else if (unitFrom === "mile" && unitTo === "m") {
-    result = inputValue * 1609.344; 
-  } else if (unitFrom === "mile" && unitTo === "km") {
-    result = inputValue * 1.609; 
-  } else if (unitFrom === "mile" && unitTo === "inch") {
-    result = inputValue * 63360; 
-  } else if (unitFrom === "mile" && unitTo === "ft") {
-    result = inputValue * 5280; 
-  } else if (unitFrom === "mile" && unitTo === "yard") {
-    result = inputValue * 1760;  
-  } else if (unitFrom === "mile" && unitTo === "nauticalMile") {
-    result = inputValue / 1.151; 
-  } else if (unitFrom === "nauticalMile" && unitTo === "cm") {
-    result = inputValue * 185200; 
-  } else if (unitFrom === "nauticalMile" && unitTo === "m") {
-    result = inputValue * 1852;
-  } else if (unitFrom === "nauticalMile" && unitTo === "km") {
-    result = inputValue * 1.852; 
-  } else if (unitFrom === "nauticalMile" && unitTo === "inch") {
-    result = inputValue * 72913.386; 
-  } else if (unitFrom === "nauticalMile" && unitTo === "ft") {
-    result = inputValue * 6076.115; 
-  } else if (unitFrom === "nauticalMile" && unitTo === "yard") {
-    result = inputValue * 2025.372; 
-  } else if (unitFrom === "nauticalMile" && unitTo === "mile") {
-    result = inputValue * 1.151; 
-  } else {
-    result = inputValue;
-  }
+var InputTypeValue, ResultTypeValue,OptionTypeValue;
 
-  
-  document.getElementById("result").innerHTML = result.toFixed(2);
+OptionType.addEventListener("change",myResult);
+input.addEventListener("keyup", myResult);
+InputType.addEventListener("change", myResult);
+ResultType.addEventListener("change", myResult);
+
+InputTypeValue = InputType.value;
+ResultTypeValue = ResultType.value;
+OptionTypeValue = OptionType.value;
+
+function myResult() {
+
+    // when u change the input and output value we need to update the InputTypeValue and ResultTypeValue
+    InputTypeValue = InputType.value;
+    ResultTypeValue = ResultType.value;
+
+    // Now compare the input and output resultType value and add formula
+
+    // Length Converter
+    
+    if (InputTypeValue ==="meter" && ResultTypeValue ==="kilometer") {
+        result.value = Number(input.value) * 0.001;
+    } else if (InputTypeValue === "meter" && ResultTypeValue === "centimeter") {
+        result.value = Number(input.value) * 100;
+    }
+    else if (InputTypeValue === "meter" && ResultTypeValue === "meter") {
+        result.value = input.value;
+    }
+
+    if (InputTypeValue === "kilometer" && ResultTypeValue === "meter") {
+        result.value = Number(input.value) * 1000;
+    }
+    else if (InputTypeValue === "kilometer" && ResultTypeValue === "centimeter") {
+        result.value = Number(input.value) * 100000;
+    }
+    else if (InputTypeValue === "kilometer" && ResultTypeValue === "kilometer") {
+        result.value = input.value;
+    }
+
+    if (InputTypeValue === "centimeter" && ResultTypeValue === "meter") {
+        result.value = Number(input.value) * 0.00001;
+    }
+    else if (InputTypeValue === "centimeter" && ResultTypeValue === "kilometer") {
+        result.value = Number(input.value) * 0.01;
+    }
+    else if (InputTypeValue === "centimeter" && ResultTypeValue === "centimeter") {
+        result.value = input.value;
+    }
+    
+    // Tempreature Converter
+    if (InputTypeValue === "Degree Celsius" && ResultTypeValue === "Fahrenheit") {
+        result.value = Number(input.value) * 1.8+32;
+    }
+    else if (InputTypeValue === "Degree Celsius" && ResultTypeValue === "Kelvin") {
+        result.value = Number(input.value) + 273.15;
+    }
+    else if (InputTypeValue === "Degree Celsius" && ResultTypeValue === "Degree Celsius") {
+        result.value = input.value;
+    }
+    if (InputTypeValue === "Fahrenheit" && ResultTypeValue === "Degree Celsius") {
+        result.value = ((Number(input.value)- 32) * (5/9));
+    }
+    else if (InputTypeValue === "Fahrenheit" && ResultTypeValue === "Kelvin") {
+        result.value = (Number(input.value) + 459.67)*(5/9);
+    }
+    else if (InputTypeValue === "Fahrenheit" && ResultTypeValue === "Fahrenheit") {
+        result.value = input.value;
+    }
+    if (InputTypeValue === "Kelvin" && ResultTypeValue === "Degree Celsius") {
+        result.value = (Number(input.value)-273.15);
+    }
+    else if (InputTypeValue === "Kelvin" && ResultTypeValue === "Fahrenheit") {
+        result.value = ((9/5)*(Number(input.value)-273) + 32);
+    }
+    else if (InputTypeValue === "Kelvin" && ResultTypeValue === "Kelvin") {
+        result.value = input.value;
+    }
+
+    // Weight Converter
+    // Kilogram to gram,Miligram,Microgram,Pound
+
+    if (InputTypeValue === "Kilogram" && ResultTypeValue === "Gram") {
+        result.value = (Number(input.value) * 1000);
+    }
+    else if (InputTypeValue === "Kilogram" && ResultTypeValue === "Miligram") {
+        result.value = (Number(input.value) * 1000000);
+    }
+    else if (InputTypeValue === "Kilogram" && ResultTypeValue === "Microgram") {
+        result.value = (Number(input.value) * 1000000000);
+    }
+    else if (InputTypeValue === "Kilogram" && ResultTypeValue === "Pound") {
+        result.value = (Number(input.value) * 2.205);
+    }
+    else if (InputTypeValue === "Kilogram" && ResultTypeValue === "Kilogram") {
+        result.value = input.value;
+    }
+
+    // Gram to kilogram,miligram,microgram,pound
+    if (InputTypeValue === "Gram" && ResultTypeValue === "Kilogram") {
+        result.value = (Number(input.value) / 1000);
+    }
+    else if (InputTypeValue === "Gram" && ResultTypeValue === "Miligram") {
+        result.value = (Number(input.value) * 1000);
+    }
+    else if (InputTypeValue === "Gram" && ResultTypeValue === "Microgram") {
+        result.value = (Number(input.value) * 1000000);
+    }
+    else if (InputTypeValue === "Gram" && ResultTypeValue === "Pound") {
+        result.value = (Number(input.value) / 453.6);
+    }
+    else if (InputTypeValue === "Gram" && ResultTypeValue === "Gram") {
+        result.value = input.value;
+    }
+
+    // miligram to kilogram,gram,microgram,pound
+    if (InputTypeValue === "Miligram" && ResultTypeValue === "Kilogram") {
+        result.value = (Number(input.value) / 1000000);
+    }
+    else if (InputTypeValue === "Miligram" && ResultTypeValue === "Gram") {
+        result.value = (Number(input.value) / 1000);
+    }
+    else if (InputTypeValue === "Miligram" && ResultTypeValue === "Microgram") {
+        result.value = (Number(input.value) * 1000);
+    }
+    else if (InputTypeValue === "Miligram" && ResultTypeValue === "Pound") {
+        result.value = (Number(input.value) / 453600);
+    }
+    else if (InputTypeValue === "Miligram" && ResultTypeValue === "Miligram") {
+        result.value = input.value;
+    }
+
+    // Microgram to kilogram,gram,miligram,pound
+    if (InputTypeValue === "Microgram" && ResultTypeValue === "Kilogram") {
+        result.value = (Number(input.value) / 1000000000);
+    }
+    else if (InputTypeValue === "Microgram" && ResultTypeValue === "Gram") {
+        result.value = (Number(input.value) / 1000000);
+    }
+    else if (InputTypeValue === "Microgram" && ResultTypeValue === "Miligram") {
+        result.value = (Number(input.value) / 1000);
+    }
+    else if (InputTypeValue === "Microgram" && ResultTypeValue === "Pound") {
+        result.value = ((Number(input.value) * (2.205))/1000000000);
+    }
+    else if (InputTypeValue === "Microgram" && ResultTypeValue === "Microgram") {
+        result.value = input.value;
+    }
+
+    // Pound to kilogram,gram,miligram,microgram,pound
+    if (InputTypeValue === "Pound" && ResultTypeValue === "Kilogram") {
+        result.value = (Number(input.value) / 2.205);
+    }
+    else if (InputTypeValue === "Pound" && ResultTypeValue === "Gram") {
+        result.value = (Number(input.value) * 453.6);
+    }
+    else if (InputTypeValue === "Pound" && ResultTypeValue === "Miligram") {
+        result.value = (Number(input.value) * 453600);
+    }
+    else if (InputTypeValue === "Pound" && ResultTypeValue === "Microgram") {
+        result.value = ((Number(input.value) * 453592370));
+    }
+    else if (InputTypeValue === "Pound" && ResultTypeValue === "Pound") {
+        result.value = input.value;
+    }
+
+    // Time Converter
+    // Minute to hour,second
+    if (InputTypeValue === "Minute" && ResultTypeValue === "Hour") {
+        result.value = (Number(input.value) / 60);
+    }
+    else if (InputTypeValue === "Minute" && ResultTypeValue === "Second") {
+        result.value = (Number(input.value) * 60);
+    }
+    else if (InputTypeValue === "Minute" && ResultTypeValue === "Minute") {
+        result.value = input.value;
+    }
+
+    // Hour to minute,second
+    if (InputTypeValue === "Hour" && ResultTypeValue === "Minute") {
+        result.value = (Number(input.value) * 60);
+    }
+    else if (InputTypeValue === "Hour" && ResultTypeValue === "Second") {
+        result.value = (Number(input.value) * 3600);
+    }
+    else if (InputTypeValue === "Hour" && ResultTypeValue === "Hour") {
+        result.value = input.value;
+    }
+
+    // Second to minute,hour
+    if (InputTypeValue === "Second" && ResultTypeValue === "Minute") {
+        result.value = (Number(input.value) / 60);
+    }
+    else if (InputTypeValue === "Second" && ResultTypeValue === "Hour") {
+        result.value = (Number(input.value) * 3600);
+    }
+    else if (InputTypeValue === "Second" && ResultTypeValue === "Second") {
+        result.value = input.value;
+    }
 }
